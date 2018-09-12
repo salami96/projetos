d447a2c61a99bc631a96238a6de1aaf2616bb170
Gabriel Zanatto Salami <gabriel.zanatto2@gmail.com>
2018-09-12 17:25:01 -0300

    mongo db native running :D

--- index.ts
+++ index.ts
@@ -1,2 +1,17 @@
-let saudacao: string = 'Alô, mundo! Now for github2';
-console.log(saudacao);
+import * as mongodb from 'mongodb';
+const url = 'mongodb://localhost:27017/teste';
+
+async function main() {
+    try {
+        const cli = await mongodb.MongoClient.connect(url,{useNewUrlParser: true});
+        const db = cli.db('teste');
+        console.log("conectou :D");
+        const colecao = db.collection('exemplos');
+        const result = await colecao.insertOne({name:'Jhon Doe'});
+        console.log(`InsertId: ${result.insertedId}`);
+                
+    } catch(erro) {
+        console.log(erro);
+    }
+}
+main();
