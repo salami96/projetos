e0b20d6f7616fe0377a75d03aea2c0976b3cad6d
Gabriel Zanatto Salami <gabriel.zanatto2@gmail.com>
2018-09-13 16:57:11 -0300

    mongoose install

--- index.ts
+++ index.ts
@@ -1,21 +1,34 @@
-import * as mongodb from 'mongodb';
-const url = 'mongodb://localhost:27017/teste';
+//import * as mongodb from 'mongodb';
+//const url = 'mongodb://localhost:27017/teste';
+import * as mongoose from 'mongoose';
+const url = 'mongodb://localhost:27017/testemongoose';
 
-async function main() {
+/*async function main() {
     try {
         const cli = await mongodb.MongoClient.connect(url,{useNewUrlParser: true});
         const db = cli.db('teste');
         console.log("conectou :D");
         const colecao = db.collection('exemplos');
-        const result = await colecao.insertOne({fruta:'chuchu'});
-        console.log(`InsertId: ${result.insertedId}`);
-        const cursor = colecao.find({})
-        while(await cursor.hasNext()){
-            let documento = await cursor.next();
-            console.log(documento);
-        }
+        //const result = await colecao.insertOne({fruta:'chuchu'});
+        //console.log(`InsertId: ${result.insertedId}`);
+        const cursor = colecao.find({});
+        let array = await cursor.toArray();
+        console.log(array);
+        let res = await colecao.updateOne({name:'nicole'},{$set:{idade:19}});
+        console.log(res.result);
+        let resRemov = await colecao.deleteOne({fruta:{$exists:true}});
+        console.log("remoção: "+resRemov);
+        await cli.close;               
     } catch(erro) {
         console.log(erro);
     }
+}
+main();
+*/
+
+async function main (){
+    const cli = await mongoose.connect(url,{useNewUrlParser: true});
+    console.log('conectou');
+    
 }
 main();
